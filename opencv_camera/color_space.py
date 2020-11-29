abcef70941e894f6aceae4393518d3701d4b2a3e
walchko <walchko@users.noreply.github.com>
2020-11-29 08:19:38 -0500

    up

--- opencv_camera/color_space.py
+++ opencv_camera/color_space.py
@@ -12,5 +12,10 @@ from enum import IntFlag
 ColorSpace = IntFlag("ColorSpace", "bgr rgb hsv gray")
 
 bgr2rgb = lambda im: cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+rgb2bgr = lambda im: cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
+
+hsv2bgr = lambda im: cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
+bgr2hsv = lambda im: cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+
 bgr2gray = lambda im: cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 gray2bgr = lambda im: cv2.cvtColor(im, cv2.COLOR_GRAY2BGR)
